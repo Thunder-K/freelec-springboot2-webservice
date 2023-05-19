abb318bcce45e85c4abb0ced424f4896aae77dde
taehwankim-7244 <rlaxo7244@gmail.com>
2023-05-19 18:10:45 +0900

    AWS를 이용한 웹 서비스 구축

--- src/main/java/com/jojo/book/springboot/Application.java
+++ src/main/java/com/jojo/book/springboot/Application.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 //@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
+    //aws 서버 설정중
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
